953acaf9fb1088505d2283ab5999b5dc15db5aa4
OtomeSound <s890081tonyhsu@gmail.com>
2013-11-14 23:43:23 +0800

    Put the font on justfont
    1.	Import justfont's js file
    2.	Invisible the justfont logo.

--- data/config.rb
+++ data/config.rb
@@ -5,7 +5,7 @@ http_path = "/"
 css_dir = "css"
 sass_dir = "scss"
 images_dir = "images"
-javascripts_dir = "javascripts"
+javascripts_dir = "js"
 relative_assets = true
 cache = false
 
---
+++ data/js/mainpage.js
@@ -0,0 +1,4 @@
+function mm_includejs (jsFile){
+	document .write('<script type="text/javascript" src="data/js/' +jsFile + '"></script>');
+}
+mm_includejs('font.js');
