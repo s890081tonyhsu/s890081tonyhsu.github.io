aa0de14561bb02aafee4c316407c1b5231eca917
OtomeSound <s890081tonyhsu@gmail.com>
2014-02-05 23:27:07 +0800

    Change Background and reset css.
    1.	Change Background.
    2.	Import jquery.
    3.	Reset css.

--- data/js/mainpage.js
+++ data/js/mainpage.js
@@ -2,3 +2,16 @@ function mm_includejs (jsFile){
 	document .write('<script type="text/javascript" src="data/js/' +jsFile + '"></script>');
 }
 mm_includejs('font.js');
+
+function checkXY(){
+	var y = $(window).height();
+	var x = $(window).width();
+	if(x > y){
+		$("body").addClass("xy");
+	}else{
+		$("body").addClass("yx");
+	}
+}
+
+$(document).ready(checkXY);
+$(window).resize(checkXY);
