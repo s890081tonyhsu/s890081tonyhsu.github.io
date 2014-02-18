de89afcbe8473d44c46b2fd3c50914655e8c57f9
OtomeSound <s890081tonyhsu@gmail.com>
2014-02-19 06:06:47 +0800

    Clearify the github api.
    1.	Clean the js code to function.
    2.	Open github access_token.

--- data/js/sub/works.js
+++ data/js/sub/works.js
@@ -1,18 +1,45 @@
-function GetData(response){
-		var detail = response.data;
-		console.log(detail);
-		detail.forEach(function(repo){
+function GetData(Detail){
+	return $.ajax({
+		type: 'GET',
+		dataType: 'jsonp',
+		data: {},
+		url: "https://api.github.com/users/s890081tonyhsu/"+Detail+"?access_token=70b8535d3f4022a39aa9859d5d79e7c1de40df73",
+		error: function (jqXHR, textStatus, errorThrown) {
+				console.log(jqXHR);
+		},
+		success: function (msg) {
+				console.log(msg);
+		},
+	});
+}
+
+function ShowList(data){
+		console.log(data);
+		data.data.forEach(function(repo){
 			$('#repo').append('<div id="'+repo.id+'" onclick="window.open(\''+repo.html_url+'\',\'_blank\');" style="cursor:pointer;"></div>');
-			$('#'+repo.id).append('<h4>'+repo.full_name.replace("s890081tonyhsu/","")+'</h4></a>');
-			$('#'+repo.id).append('<p>'+repo.language+'</p>');
+			$('#'+repo.id).append('<h4>'+repo.name+'</h4></a>');
+			$.ajax({
+				type: 'GET',
+				dataType: 'jsonp',
+				data: {},
+				url: repo.languages_url+"?access_token=70b8535d3f4022a39aa9859d5d79e7c1de40df73",
+				error: function (jqXHR, textStatus, errorThrown) {
+					console.log(jqXHR);
+				},
+				success: function (msg) {
+					var str = [];
+					console.log(msg);
+					$.each(msg.data, function(idx){
+						str.push(idx);
+					});
+					$('#'+repo.id).append('<p>'+str.join(", ")+'</p>');
+				},
+			});
 			if(repo.fork == true){
 				$('#'+repo.id).addClass('forked');
 			}	
 		});
 		checkXY(true);
-	}
-	
-var script = document.createElement('script');
-script.src = 'https://api.github.com/users/s890081tonyhsu/repos?callback=GetData';
+}
 
-document.getElementsByTagName('head')[0].appendChild(script);
+$(document).ready(GetData("repos").done(ShowList));
