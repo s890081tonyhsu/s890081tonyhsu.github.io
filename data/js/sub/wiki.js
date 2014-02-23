33de1fa4d7447498fefa29996ff26cff9638a99c
OtomeSound <s890081tonyhsu@gmail.com>
2014-02-24 00:59:55 +0800

    Use myself api to get data from dokuwiki.
    1.	Write down api from server side.
    2.	Use ajax to get json data.
    3.	Let js to output index.
    
    It's better than YQL to get less data.

---
+++ data/js/sub/wiki.js
@@ -0,0 +1,40 @@
+WikiPrefix = 'http://nchusg.org/~otomesound/dokuwiki/doku.php?id=';
+function isNumber(n) {
+		return !isNaN(parseFloat(n)) && isFinite(n);
+}
+function Console_Dir(path,data,div){
+		var name = new Array();
+		for(var i in data){
+				if(isNumber(i))continue;
+				else name.push(i);
+		}
+		for(var i in data){
+				if(!isNumber(i))continue;
+				else name.push(i);
+		}
+		for(var key in name){
+				if(!isNumber(name[key])){
+						$(div).append('<li onclick="$(\'#'+name[key]+'\').slideToggle();"><strong>'+name[key]+'</strong></li><ul id="'+name[key]+'" class="sub" style="display:none;"></ul>');
+						Console_Dir(path+key+':',data[name[key]],'#'+name[key]);
+				}else{
+						output = data[name[key]].replace('.txt','');
+						$(div).append('<li><a href="'+WikiPrefix+path+output+'">'+output+'</a></li>');
+				}
+		}
+}
+function GetData(){
+		return $.ajax({
+				type: 'GET',
+			   data: {},
+			   url: "http://nchusg.org/~otomesound/dokuwiki/api/dir_test.php",
+			   error: function (jqXHR, textStatus, errorThrown) {
+					   console.log(jqXHR);
+			   },
+			   success: function (msg) {
+					   var get = msg;
+					   console.log(get);
+					   Console_Dir('',get,'#index');
+			   },
+		});
+}
+var test = GetData();
