b2cdc96a0c3653d18dfebdee5ac44b838e31523b
OtomeSound <s890081tonyhsu@gmail.com>
2014-11-07 11:46:12 +0800

    Work Page second version.
    1. Add stat section, change repo section view.
    2. Sassing the menu css.

--- assets/js/sub/works.js
+++ assets/js/sub/works.js
@@ -14,14 +14,25 @@ function GetData(Detail){
 }
 
 function ShowList(data){
-		console.log(data);
+		var langStat = {};
 		data.data.forEach(function(repo){
-			$('#repo').append('<div id="'+repo.id+'" onclick="window.open(\''+repo.html_url+'\',\'_blank\');" style="cursor:pointer;"></div>');
-			$('#'+repo.id).append('<h4>'+repo.name+'</h4></a>');
+			$('#repo').append('<div id="'+repo.id+'"></div>');
+			$('#'+repo.id).append('<h4>'+repo.name+'</h4>');
+			var lang = repo.language || "Text"
+			$('#'+repo.id).append('<p class="lang '+ lang.toLowerCase() +'">'+ lang +'</p>');
+			if(langStat[lang] == null) langStat[lang] = 1;
+			else langStat[lang]++;
 			if(repo.fork == true){
 				$('#'+repo.id).addClass('forked');
-			}	
+				langStat[lang]--;
+			}
+			var description = repo.description || "This repo has no description to tell you something.";
+			$('#'+repo.id).append('<h6>'+description+'</h6>');
+			$('#'+repo.id).append('<a href="' + repo.html_url + '"><i class="link"></a>');
 		});
+		for (var key in langStat) {
+			$('#stat').append(' <p><span class="stat ' + key.toLowerCase() + '">' + key + '</span> x ' + langStat[key] + '</p>');
+		};
 }
 
 $(document).ready(GetData("repos").done(ShowList));
