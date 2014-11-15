a24cadd33eae416877f5a622ec69ab204e0d38cf
OtomeSound <s890081tonyhsu@gmail.com>
2014-11-15 09:41:50 +0800

    Update AboutMe and Repo language
    1.	About me update without profile
    2.	Repo Language add Python and shell, add icon brfore name.

--- assets/js/sub/about.js
+++ assets/js/sub/about.js
@@ -1,15 +1,9 @@
-$('section').click(function(){
-		var key = $(this).attr('name');
-		var visible = $('div[name]:visible');
-		$(this).siblings().fadeOut();
-		if($(visible).attr("name") != key){
-				$('div[name]').fadeOut().queue(function(){
-						$('div[name='+key+']').fadeIn().dequeue();
-				});
-		}else return;
-});
-
-$('div.return').click(function(){
-	$('section').each(function(){$(this).fadeIn();});
-	$('div[name]:visible').fadeOut();
+$('.section li').each(function(){
+	var overlay = $(this).find('.overlay');
+	$(this).bind('click', function(){
+		overlay.css({"opacity": 1, "z-index": 9999, "pointer-events": "auto"});
+	});
+	$(this).find('.fa-times').bind('click', function(){
+		overlay.css({"opacity": 0, "z-index": -1, "pointer-events": "none"});
+	});
 });
--- assets/js/sub/works.js
+++ assets/js/sub/works.js
@@ -18,20 +18,22 @@ function ShowList(data){
 		data.data.forEach(function(repo){
 			$('#repo').append('<div id="'+repo.id+'"></div>');
 			$('#'+repo.id).append('<h4>'+repo.name+'</h4>');
+			if(repo.fork == true){
+				$('#'+repo.id+' h4').prepend('<i class="fa fa-code-fork"></i>&nbsp;');
+			}else{
+				$('#'+repo.id+' h4').prepend('<i class="fa fa-book"></i>&nbsp;');
+			}
 			var lang = repo.language || "Text"
 			$('#'+repo.id).append('<p class="lang '+ lang.toLowerCase() +'">'+ lang +'</p>');
 			if(langStat[lang] == null) langStat[lang] = 1;
 			else langStat[lang]++;
-			if(repo.fork == true){
-				$('#'+repo.id).addClass('forked');
-				langStat[lang]--;
-			}
 			var description = repo.description || "This repo has no description to tell you something.";
 			$('#'+repo.id).append('<h6>'+description+'</h6>');
 			$('#'+repo.id).append('<a href="' + repo.html_url + '"><i class="link"></a>');
 		});
+		$('#stat p').append('<span>(Repo x '+ data.data.length +')</span>');
 		for (var key in langStat) {
-			$('#stat').append(' <p><span class="stat ' + key.toLowerCase() + '">' + key + '</span> x ' + langStat[key] + '</p>');
+			$('#stat').append(' <p><span class="stat ' + key.toLowerCase() + '">' + key + '</span> ' + Math.floor((langStat[key]/data.data.length)*100) + '% ('+langStat[key]+'/'+data.data.length+')</p>');
 		};
 }
 
