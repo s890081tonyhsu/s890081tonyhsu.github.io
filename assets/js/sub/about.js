609aaf850d311037fac9347a1d7493a099129a59
OtomeSound <s890081tonyhsu@gmail.com>
2014-11-15 12:09:23 +0800

    Close click function.

--- assets/js/sub/about.js
+++
@@ -1,9 +0,0 @@
-$('.section li').each(function(){
-	var overlay = $(this).find('.overlay');
-	$(this).bind('click', function(){
-		overlay.css({"opacity": 1, "z-index": 9999, "pointer-events": "auto"});
-	});
-	$(this).find('.fa-times').bind('click', function(){
-		overlay.css({"opacity": 0, "z-index": -1, "pointer-events": "none"});
-	});
-});
