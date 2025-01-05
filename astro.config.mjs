476d9546316eeaf93d2e2523f3fcb8cbeb6eac9d
OtomeSound <s890081tonyhsu@gmail.com>
2025-01-05 15:33:32 +0800

    fix: github does not serve folders with underscore prefix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,11 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    // NOTE: github doesn't build folders with `_` prefix.
+    // Ref: https://docs.github.com/en/pages/setting-up-a-github-pages-site-with-jekyll/about-github-pages-and-jekyll#configuring-jekyll-in-your-github-pages-site
+    assets: 'assets',
+  },
   site: 'https://s890081tonyhsu.github.io',
   integrations: [vue(), mdx()],
   vite: {
